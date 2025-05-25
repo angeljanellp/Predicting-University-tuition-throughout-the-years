e03894797bf7d94fd52b61bfeb3b13553d3513a5
Angelie Pasamonte <165096210+angeljanellp@users.noreply.github.com>
2025-05-25 05:30:28 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from explore_page import show_explore_page
+from predict_page import show_predict_page
+
+page=st.sidebar.selectbox('Explore or Predict',('Predict', 'Explore'))
+
+if page== "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
